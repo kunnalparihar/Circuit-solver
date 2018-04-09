60acb4e8fe92f2fd2de31ae9117b766f49a75f08
Kunal Parihar <32226132+kunnalparihar@users.noreply.github.com>
2018-04-09 20:02:59 +0530

    Update and rename Assignment2cop.java to CircuitSolver_Main.java

--- CircuitSolver_Main.java
+++ CircuitSolver_Main.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Assignment2cop extends DrawComponentssvg  {
+public class CircuitSolver_Main extends DrawComponentssvg  {
 
     /**
      * @param args the command line arguments
